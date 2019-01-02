624f81c941b30055268b56fbbd0c5f8f27598021
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2019-01-02 23:42:16 +0200

    Update task5.js

--- js/task5.js
+++ js/task5.js
@@ -1,38 +1,38 @@
 function ask(question, answer, ok, fail) {
-	var result = prompt(question, '');
-	if (result === answer) {
-		ok();
-	} else {
-		fail();
-	}
+  var result = prompt(question, '');
+  if (result === answer) {
+    ok();
+  } else {
+    fail();
+  }
 }
 var user = {
-	login: 'Andrew',
-	password: '12345',
+  login: 'Andrew',
+  password: '12345',
 
-	loginOk: function() {
-		console.log(this.login + ' - login success');
-	},
+  loginOk: function() {
+    console.log(this.login + ' - login success');
+  },
 
-	loginFail: function() {
-		console.log(this.login + ' - login failed');
+  loginFail: function() {
+    console.log(this.login + ' - login failed');
   },
   // bind
-	checkPassword: function() {
-		ask ("Your password?", this.password, this.loginOk.bind(this), this.loginFail.bind(this));
-  },
-	//cloused
   checkPassword: function() {
-		var context = this;
+    ask ("Your password?", this.password, this.loginOk.bind(this), this.loginFail.bind(this));
+  },
+  //cloused
+  checkPassword: function() {
+    var context = this;
     ask ("Your password?", context.password,  
       function() {
-         return context.loginOk()
+        return context.loginOk()
       },
       function() {
         return context.loginFail()
       }
     )
-	}
+  }
 };
 
 user.checkPassword();
