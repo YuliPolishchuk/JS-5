5afabdad1aecd5aa921d044625a60609771a62c5
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2019-01-03 20:28:23 +0200

    Update task1.js

--- js/task1.js
+++ js/task1.js
@@ -1,9 +1,10 @@
 var context  = {
-  name : 'name',
-  number : 2
+  name: 'name',
+  number: 2
 };
 
 console.log(getName());
+console.log(getName.call(context));
 
 function getName() {
   return this.context;
