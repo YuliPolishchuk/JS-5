6cb7584c962161279d150edb3161d58be386cc26
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2019-01-08 19:46:51 +0200

    Update task6.js

--- js/task6.js
+++ js/task6.js
@@ -1,27 +1,28 @@
 ;(function() {
-    var img = [{image: 'car.jpg', alt: 'Car'}, {image: 'apple.jpg', alt: 'Apple'}];
-    var slideIndex = 0;
+  var img = [{image: 'car.jpg', alt: 'Car'}, {image: 'apple.jpg', alt: 'Apple'}];
+  var slideIndex = 0;
 
-    console.log(showSlide(1));
-    console.log(getInform());
-    console.log(switchSlideNumber(1));
+  console.log(showSlide(false));
+  console.log(showSlide());
+  console.log(getInform());
+  console.log(switchSlideNumber(1));
 
-    function showSlide(next = true) {
-      var slide = next ? slideIndex++ : slideIndex--;
-      slideIndex = Math.max(0, Math.min(img.length - 1, slide));
-      return slideIndex;
-    }
+  function showSlide(next = true) {
+    next ? slideIndex++ : slideIndex--;
+    slideIndex = Math.max(0, Math.min(img.length - 1, slideIndex));
+    return slideIndex;
+  }
 
-    function switchSlideNumber(n) {
-      if (n <= img.length - 1) {
-        slideIndex = n;
-        return img[slideIndex];
-      }
-      slideIndex = img.length;
+  function switchSlideNumber(n) {
+    if (n <= img.length - 1) {
+      slideIndex = n;
       return img[slideIndex];
     }
-    
-    function getInform() {
-      return img[slideIndex];
-    }
+    slideIndex = img.length;
+    return img[slideIndex];
+  }
+  
+  function getInform() {
+    return img[slideIndex];
+  }
 }());
