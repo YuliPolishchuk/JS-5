6786a4062d5af8d4bdfb39c1388b9d373a6d9ad4
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2019-01-22 19:33:07 +0200

    Update task6.js

--- js/task6.js
+++ js/task6.js
@@ -2,11 +2,31 @@
   var img = [{image: 'car.jpg', alt: 'Car'}, {image: 'apple.jpg', alt: 'Apple'}];
   var slideIndex = 0;
 
-  console.log(showSlide(false));
-  console.log(showSlide());
+  /*console.log(showSlide(false));
+  console.log(showSlide());*/
+  console.log(next());
+  console.log(previos());
   console.log(getInform());
   console.log(switchSlideNumber(1));
 
+  function next() {
+    if (slideIndex < img.length - 1) {
+      slideIndex++;
+    } else {
+      slideIndex = 0;
+    }
+    return img[slideIndex];
+  }
+  
+  function previos() {
+    if (slideIndex>= img.length - 1) {
+      slideIndex--;
+    } else {
+      slideIndex = img.length - 1;
+    }
+    return img[slideIndex];
+  }
+  /*
   function showSlide(next = true) {
     if (next) {
       slideIndex++;
@@ -15,7 +35,7 @@
     }
     slideIndex = Math.max(0, Math.min(img.length - 1, slideIndex));
     return slideIndex;
-  }
+  }*/
 
   function switchSlideNumber(n) {
     if (n <= img.length - 1) {
